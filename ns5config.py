74187a5f22fae2ff0fc5e0c913db0f069e19ae56
Ryuji Masuda <rjmasuda@gmail.com>
2019-08-19 15:53:57 -0700

    Sample disk path count.

--- ns5config.py
+++ ns5config.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import lib.ns5request as ns5request
 import lib.multipart_encoder as multipart_encoder
+import json
 
-HOST="192.168.77.128"
+HOST="10.0.100.91"
 PORT="8443"
 USER="admin"
 PASS="nexentA01"
@@ -12,8 +13,10 @@ NODE= HOST + ":" + PORT
 
 conn = ns5request.Ns5request()
 conn.ns5login(NODE, USER, PASS)
-print(conn.ns5get("/network/links"))
-
+jsonOutput = conn.ns5get("/inventory/disks?limit=250")
+diskInfo = json.loads(jsonOutput)
+for disk in diskInfo['data']:
+  print(disk['logicalDevice'], "OnlinePhyCount:",  disk['multipath']['onlinePhyCount'])
 
 #headers, body = multipart_encoder.multipart_encoder({"license":""},{"license":"license.txt"})
 #print(headers)
